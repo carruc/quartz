8a8a9e4ba0e534f6a3c761e00fcb83648a570b47
Pietro Carrucciu <76063704+carruc@users.noreply.github.com>
2025-07-11 10:47:43 +0200

    Update PageTitle.tsx

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -6,9 +6,10 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+      <a href={baseDir} onClick="window.location.href = this.href; return false;">{title}</a>
     </h2>
   )
 }
@@ -27,7 +28,5 @@ PageTitle.css = `
   flex: 1;
 }
 `
-
-
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
